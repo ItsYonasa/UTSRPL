392a9754918c97b574856733748273c16320ddc5
ItsYonasa <yosephp.s1412@gmail.com>
2021-09-27 09:56:07 +0700

    Senin, 27 September 2021

---
+++ World.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>UTSRPL</h1>
+<h2>Senin, 27 September
+2021</h2>
+<?php
+echo "Yoseph Prajna Sanjaya";
+echo "17190051";
+echo "Hello World, UTS RPL 2021";
+?>
+
+</body>
+</html>
